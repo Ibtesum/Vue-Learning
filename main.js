16c44f20a202de86f03256da4e5f43cfffbb4cde
Ibtesum <aninda7152@gmail.com>
2023-12-10 16:08:25 +0600

    initailizing app

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: "Sock",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, labore."
+        }
+    }
+})
