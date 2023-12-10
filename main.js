11004bd42ce4885b11ea585862b5ca16ae5e1d96
Ibtesum <aninda7152@gmail.com>
2023-12-10 16:52:52 +0600

    Attribute binding and conditional rendering

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data(){
         return {
             product: "Sock",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, labore."
+            image: "./assets/images/socks_green.jpg",
+            url: "https://www.google.com/",
+            inStock: false,
+            inventory: 0,
+            onSale: true
         }
     }
 })
